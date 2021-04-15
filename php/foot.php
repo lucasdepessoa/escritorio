bb69542ff1d273fcebbf071e2a49962d3a9fa484
Lucas Fernandes Pessoa <lucas192380@br-atacadao.corp>
2021-04-15 01:56:37 -0300

    footer dinamico

---
+++ php/foot.php
@@ -0,0 +1,58 @@
+<footer id="fh5co-footer" role="contentinfo">
+    <div class="container">
+        <div class="row row-pb-md">
+            <div class="col-md-3 fh5co-widget">
+                <h4>Frase de escolha ou menus adicionais</h4>
+                <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+            </div>
+            <div class="col-md-3 col-md-push-1">
+                <h4>Navegação</h4>
+                <ul class="fh5co-footer-links">
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./practice.html">Especialidades</a></li>
+                    <li><a href="./won.html">Consulta</a></li>
+                    <li><a href="./blog.html">Blog</a></li>
+                    <li><a href="./about.html">Sobre Nós</a></li>
+                    <li><a href="./contact.html">Calculadora</a></li>
+                    <li><a href="./admin.php">Área Administrativa</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-md-push-1">
+                <h4>Informações de Contato</h4>
+                <ul class="fh5co-footer-links">
+                    <li>R. Couto Magalhães 2073, <br> Centro Franca - São Paulo</li>
+                    <li><a href="tel://1234567920">+55 (16) 3721-7940</a></li>
+                    <li><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
+                    <!-- <li><a href="http://gettemplates.co">gettemplates.co</a></li> -->
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-md-push-1">
+                <h4>Horário de Atendimento</h4>
+                <ul class="fh5co-footer-links">
+                    <li>Segunda-feira - Sexta-feira: 9:00 - 18:00</li>
+                    <!-- <li>Fri 8:00 - 21 00</li>
+                    <li>Sat 9:30 - 15: 00</li> -->
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="row copyright">
+            <div class="col-md-12 text-center">
+                <p>
+                    <small class="block">&copy; 2021 Alive Oliveira Advocacia. All Rights Reserved.</small> 
+                </p>
+                <p>
+                    <ul class="fh5co-social-icons">
+                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        <li><a href="#"><i class="icon-dribbble"></i></a></li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
--- won.php
+++ won.php
@@ -64,17 +64,17 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-xs-2">
-							<div id="fh5co-logo"><a href="index.html"><img class="brand-logo"
+							<div id="fh5co-logo"><a href="index.php"><img class="brand-logo"
 										src="./images/logo.png"><span></span></a></div>
 						</div>
 						<div class="col-xs-10 text-right menu-1">
 							<ul>
-								<li><a href="index.html">Home</a></li>
-								<li><a href="practice.html">Especialidades</a></li>
-								<li class="active"><a href="won.html">Consulta</a></li>
-								<li><a href="blog.html">Blog</a></li>
-								<li><a href="about.html">Sobre Nós</a></li>
-								<li><a href="contact.html">Calculadora</a></li>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="practice.php">Especialidades</a></li>
+								<li class="active"><a href="won.php">Consulta</a></li>
+								<li><a href="blog.php">Blog</a></li>
+								<li><a href="about.php">Sobre Nós</a></li>
+								<li><a href="contact.php">Calculadora</a></li>
 								<!-- <li class="btn-cta"><a href="#"><span>Login</span></a></li>
 							<li class="btn-cta"><a href="#"><span>Sign Up</span></a></li> -->
 							</ul>
@@ -275,65 +275,9 @@
 			</div>
 		</div>
 
-		<footer id="fh5co-footer" role="contentinfo">
-			<div class="container">
-				<div class="row row-pb-md">
-					<div class="col-md-3 fh5co-widget">
-						<h4>Frase de escolha ou menus adicionais</h4>
-						<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta
-							adipisci architecto culpa amet.</p>
-					</div>
-					<div class="col-md-3 col-md-push-1">
-						<h4>Navegação</h4>
-						<ul class="fh5co-footer-links">
-							<li><a href="./index.html">Home</a></li>
-							<li><a href="./practice.html">Especialidades</a></li>
-							<li><a href="./won.html">Consulta</a></li>
-							<li><a href="./blog.html">Blog</a></li>
-							<li><a href="./about.html">Sobre Nós</a></li>
-							<li><a href="./contact.html">Calculadora</a></li>
-						</ul>
-					</div>
-
-					<div class="col-md-3 col-md-push-1">
-						<h4>Informações de Contato</h4>
-						<ul class="fh5co-footer-links">
-							<li>R. Couto Magalhães 2073, <br> Centro Franca - São Paulo</li>
-							<li><a href="tel://1234567920">+55 (16) 3721-7940</a></li>
-							<li><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
-							<!-- <li><a href="http://gettemplates.co">gettemplates.co</a></li> -->
-						</ul>
-					</div>
-
-					<div class="col-md-3 col-md-push-1">
-						<h4>Horário de Atendimento</h4>
-						<ul class="fh5co-footer-links">
-							<li>Segunda-feira - Sexta-feira: 9:00 - 18:00</li>
-							<!-- <li>Fri 8:00 - 21 00</li>
-						<li>Sat 9:30 - 15: 00</li> -->
-						</ul>
-					</div>
-
-				</div>
-
-				<div class="row copyright">
-					<div class="col-md-12 text-center">
-						<p>
-							<small class="block">&copy; 2021 Alive Oliveira Advocacia. All Rights Reserved.</small>
-						</p>
-						<p>
-						<ul class="fh5co-social-icons">
-							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
-							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble"></i></a></li>
-						</ul>
-						</p>
-					</div>
-				</div>
-
-			</div>
-		</footer>
+		<?php
+			require('./php/foot.php');
+		?>
 
 	</div>
 
