2ae96a389eb70a946110b65fdf6b91938f36313c
Lucas Fernandes Pessoa <lucas192380@br-atacadao.corp>
2021-04-17 18:00:34 -0300

    menu superior

--- index.php
+++ index.php
@@ -51,10 +51,24 @@
 
 	</head>
 	<body>
+	<div class="menuSocio">
+		<ul class="menuSocial">
+			<li><span class="icon"><a href="https://www.facebook.com/alineoliveiraadvocacia/" target="_BLANK"><i class="fa fa-facebook"></i></a></span></li>
+			<li><span class="icon"><a href="https://www.instagram.com/alineoliveiraadvocacia/" target="_BLANK"><i class="fa fa-instagram"></i></a></span></li>
+			<li><span class="icon"><a href="https://youtube.com/channel/UCpvD5JL1bOiC8sWQW8ozc7g" target="_BLANK"><i class="fa fa-youtube"></i></a></span></li>
+			<li><span class="icon"><a href="https://www.linkedin.com/company/alineoliveiraadvocacia/" target="_BLANK"><i class="fa fa-linkedin"></i></a></span></li>
+		</ul>
+		<ul class="menuSocialDi">
+			<li><span class="icon"><a href="https://www.google.com/maps/place/R.+Couto+Magalh%C3%A3es,+2073+-+Centro,+Franca+-+SP,+14400-020/@-20.5348401,-47.4039101,19z/data=!3m1!4b1!4m5!3m4!1s0x94b0a6313da190ff:0x915a8bbf61ebbd86!8m2!3d-20.5348414!4d-47.4033629" target="_BLANK"><i class="fa fa-map-marker"></i></a> R. Couto Magalhães 2073, Centro Franca-SP</span></li>
+			<li><span class="icon"><a href="mailto:email@alineoliveira.com.br"><i class="fa fa-envelope"></i></a> email@alineoliveira.com.br</span> </li>
+			<li><span class="icon"><a href="tel://+551637217940"><i class="fa fa-phone"></i></a> (16) 3721-7940</span> </li>
+		</ul>
+	</div>
 		
 	<div class="fh5co-loader"></div>
 	
 	<div id="page">
+
 	<nav class="fh5co-nav" role="navigation">
 		<div class="top-menu">
 			<div class="container">
@@ -75,8 +89,7 @@
 							<li class="btn-cta"><a href="#"><span>Sign Up</span></a></li> -->
 						</ul>
 					</div>
-				</div>
-				
+				</div>	
 			</div>
 		</div>
 	</nav>
@@ -355,7 +368,7 @@
 		</div>
 	</div> -->
 
-	<div id="fh5co-testimonial" class="fh5co-bg-section">
+	<div id="fh5co-testimonial" class="fh5co-bg-section arPad">
 		<div class="container">
 			<div class="row animate-box">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
